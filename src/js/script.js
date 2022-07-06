fe715f5fcfdb37bb0273e2f1aa5096bca134b48c
Pepe Gonzalez <pepeglezdevega@gmail.com>
2022-07-06 23:02:11 +0200

    Sidebar parte uno

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+// Creacion de la sideBar
+const sidebar = document.getElementById('sideBar')
+function abrirSidebar() {
+    sidebar.style.width = "250px"
+}
+function cierraSidebar() {
+    
+    sidebar.style.width = "0px"
+}
